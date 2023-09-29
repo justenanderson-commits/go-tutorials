db823d722a22e4bb17b69cbc06384004a7cc0044
Justen Anderson <justenanderson@gmail.com>
2023-09-28 19:13:38 -0600

    Add Hellos function to greet multiple names.

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -14,11 +14,28 @@ func Hello(name string) (string, error) {
 	return message, nil
 }
 
+func Hellos(names []string) (map[string]string, error) {
+	// A map to associate names with messages.
+	messages := make(map[string]string)
+	// Loop through the received slices of names, calling
+	// the Hello function to get a message for each name.
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		// In the map, associate the retrieved mmessage with
+		// the name.
+		messages[name] = message
+	}
+	return messages, nil
+}
+
 func randomFormat() string {
 	formats := []string{
 		"Hi, %v. Welcome!",
 		"Great to see you, %v!",
-		"Hail, %v! Well met!"
+		"Hail, %v! Well met!",
 	}
 	return formats[rand.Intn(len(formats))]
 }
