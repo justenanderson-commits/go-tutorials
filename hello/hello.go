1c6345da6646beeb0703bab2a653fded0bc4a8d0
Justen Anderson <justenanderson@gmail.com>
2023-09-28 20:01:02 -0600

    Add Hellos function for multiple name greetings.

--- hello/hello.go
+++ hello/hello.go
@@ -9,9 +9,14 @@ import (
 func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
-	message, err := greetings.Hello("Gladys")
+
+	// A slice of names.
+	names := []string{"Gladys", "Samantha", "Darrin"}
+
+	// Request greeting messages for the names.
+	messages, err := greetings.Hellos(names)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(message)
+	fmt.Println(messages)
 }
