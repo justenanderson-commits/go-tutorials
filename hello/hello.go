c58e46243098ce63cf7b4f49de2c0dfd7d76786a
Justen Anderson <justenanderson@gmail.com>
2023-09-28 18:35:51 -0600

    Initial commit...after completing a couple tutorials lol

---
+++ hello/hello.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"example.com/greetings"
+	"fmt"
+	"log"
+)
+
+func main() {
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+	// Get a greeting message and print it.
+	message, err := greetings.Hello("")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(message)
+}
