bc3d786ef3479c7237049547bbc6a7abac614dba
Justen Anderson <justenanderson@gmail.com>
2023-09-28 18:56:58 -0600

    Add math random and remove comments

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -3,18 +3,15 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 )
 
-// Hello returns a greeting for the named person.
 func Hello(name string) (string, error) {
-	// If no name was given, return an error with a message.
-	// Return a greeting that embeds the name in a message.
+
 	if name == "" {
 		return "", errors.New("empty name")
 	}
 
-	// If a name was received, return a value that embeds the name.
-	// in a greeting message.
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
 }
