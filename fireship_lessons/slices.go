ea6d7f3e008de6fc0cc933f0e748fe0b4f67bd7a
Justen Anderson <justenanderson@gmail.com>
2023-10-02 16:15:09 -0600

    Add slices exercise.

---
+++ fireship_lessons/slices.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	// This declares and defines the array
+	languages := [9]string{"C", "Lisp", "C++", "Java", "Python",
+		"JavaScript", "Ruby", "Go", "Rust"} // <--- Note: Must include the trailing comma
+
+	// Define the slices (kinda like the slice method on JS arrays)
+	classics := languages[0:3]  // or simply languages[3]
+	modern := make([]string, 4) // len(modern) = 4
+	modern = languages[3:7]     // include index 3, exclude index 7 (just like JS)
+	new := languages[7:9]       // or simply languages[7]
+
+	fmt.Println("Classic languages: %v\n", classics)
+	fmt.Println("Modern languages: %v\n", modern)
+	fmt.Println("New languages: %v\n", new)
+}
