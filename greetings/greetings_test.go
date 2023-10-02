56c7d2719db56690825d0d4dfc5ba91121cc4822
Justen Anderson <justenanderson@gmail.com>
2023-10-01 18:56:52 -0600

    Add test TestHelloName

--- greetings/greetings_test.go
+++ greetings/greetings_test.go
@@ -4,3 +4,14 @@ import (
 	"regexp"
 	"testing"
 )
+
+// TestHelloName calls greetings.Hello with a name, checking for a valid return value.
+
+func TestHelloName(t *testing.T) {
+	name := "Gladys"
+	want := regexp.MustCompile(`\b` + name + `\b`)
+	msg, err := Hello("Gladys")
+	if !want.MatchString(msg) || err != nil {
+		t.Fatalf(`Hello("Gladys") = %q, %v, want match for %#q, nil`, msg, err, want)
+	}
+}
