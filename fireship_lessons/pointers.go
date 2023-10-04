c4a011767160a44bd671b2565a4e683b50aedd6f
Justen Anderson <justenanderson@gmail.com>
2023-10-04 17:44:34 -0600

    Add notes to pointers exercise

--- fireship_lessons/pointers.go
+++ fireship_lessons/pointers.go
@@ -2,11 +2,13 @@ package main
 
 import "fmt"
 
+// The default value for pointers is `nil` (like `null` in JS)
+
 func main() {
-	var address *int  // declares an int pointer
+	var address *int  // In this case, the * indicates the pointer type (int).
 	number := 42      // int
-	address = &number // addres stores the memory address of number
-	value := *address // dereferencing the value
+	address = &number // & is the address operator (address stores the memory address of number)
+	value := *address // In this case, the * dereferences the value to which the `address` points.
 
 	fmt.Println("address: \n", address)
 	fmt.Println("value: \n", value)
